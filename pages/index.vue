78b877b0507dcc5c8ecdc444963d1d76e2c31bdb
rej156 <ericjohnjuta@gmail.com>
2017-02-04 13:42:08 +0000

    Trying out pre-processors with nuxt.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,23 @@
-<template>
-  <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      Universal Vue.js Application Framework
-    </h1>
+<template lang="pug">
+  section.container
+    img.logo(src="../assets/img/logo.png")
+    h1.title Universal Vue.js Application Framework
+    input(v-model="message" placeholder="edit me")
+    p Message is {{message}}
     <nuxt-link class="button" to="/about">
       About page
     </nuxt-link>
-  </section>
 </template>
 
-<style scoped>
-.title
-{
-  margin: 50px 0;
-}
+<script>
+  export default {
+    data(context) {
+      return { message: '' }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  .title
+    margin: 50px 0
 </style>
