76bb7edf3c9f1f51e14309a671bbf13e82b77df9
rej156 <ericjohnjuta@gmail.com>
2017-02-17 10:16:18 +0000

    Added sass example for Richard, just npm install and npm run dev, works straight away

---
+++ pages/test-sass.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="ChangeMe">
+  <h1 class="ChangeMe2">
+    Wtf I can be scoped!
+  </h1>
+  <h2>{{ myData }}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  data({store}) {
+    return { myData: 'Hello world!' }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.ChangeMe
+  color: red
+h2
+  color: green
+</style>
